bb9896d01e37843747ec1dab8f9962a1fc9b0a4b
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-06-18 01:54:47 +0900

    implement leave slash cmd

--- pkg/ceviord/slashCmd.go
+++ pkg/ceviord/slashCmd.go
@@ -13,6 +13,10 @@ var Cmds = []*discordgo.ApplicationCommand{
 		Name:        "join",
 		Description: "voice actor join",
 	},
+	{
+		Name:        "bye",
+		Description: "voice actor disconnect",
+	},
 }
 
 func InteractionHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
@@ -33,6 +37,8 @@ func parseCommands(name string) (CommandHandler, error) {
 	switch name {
 	case "join":
 		h = new(join)
+	case "bye":
+		h = new(leave)
 	default:
 		return nil, fmt.Errorf("command `%s` is not found", name)
 	}
@@ -40,6 +46,7 @@ func parseCommands(name string) (CommandHandler, error) {
 }
 
 type join struct{}
+type leave struct{}
 
 func (j *join) handle(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	err := j.rawHandle(s, i)
@@ -84,5 +91,47 @@ func (_ *join) rawHandle(s *discordgo.Session, i *discordgo.InteractionCreate) e
 		Channel{pickedChannel: i.ChannelID, VoiceConn: voiceConn},
 		i.GuildID,
 	)
+	return nil
+}
+
+func (l *leave) handle(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	err := l.rawHandle(s, i)
+	var msg string
+	msg = "successfully leaved!"
+	if err != nil {
+		msg = fmt.Sprintln(fmt.Errorf("error in leave handler `%w`", err))
+		logger.Log(logging.WARN, fmt.Errorf("error in leave handler `%w`", err))
+	}
+	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{Content: msg},
+	})
+	if err != nil {
+		logger.Log(logging.WARN, fmt.Errorf("error in `leave` interaction respond, err is `%w`", err))
+	}
+}
+func (_ *leave) rawHandle(s *discordgo.Session, i *discordgo.InteractionCreate) error {
+	cev, err := ceviord.Channels.getChannel(i.GuildID)
+	if err != nil || cev == nil {
+		return fmt.Errorf("connection not found")
+	}
+	isJoin, err := cev.isActorJoined(s)
+	if !isJoin || cev.VoiceConn == nil {
+		return fmt.Errorf("voice actor is already disconnected")
+	}
+	defer func() {
+		if cev.VoiceConn != nil {
+			cev.VoiceConn.Close()
+			ceviord.Channels.deleteChannel(i.GuildID)
+		}
+	}()
+	err = cev.VoiceConn.Speaking(false)
+	if err != nil {
+		return fmt.Errorf("speaking falsing: %w", err)
+	}
+	err = cev.VoiceConn.Disconnect()
+	if err != nil {
+		return fmt.Errorf("disconnecting: %w", err)
+	}
 	return nil
 }
