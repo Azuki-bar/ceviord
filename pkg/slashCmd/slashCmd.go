458e5cdc400aa26b671c425cb5946964c348d0e3
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-08-01 20:35:49 +0900

    cmd定義を別ファイルに

--- pkg/slashCmd/slashCmd.go
+++ pkg/slashCmd/slashCmd.go
@@ -18,15 +18,15 @@ const (
 	pingCmdName   = "ping"
 )
 
-type SlashCmdGenerator struct {
+type Generator struct {
 	cmds []*discordgo.ApplicationCommand
 }
 
-func NewSlashCmdGenerator() *SlashCmdGenerator {
-	s := SlashCmdGenerator{cmds: cmds}
+func NewSlashCmdGenerator() *Generator {
+	s := Generator{cmds: slashCmdList}
 	return &s
 }
-func (s *SlashCmdGenerator) AddCastOpt(ps []ceviord.Parameter) error {
+func (s *Generator) AddCastOpt(ps []ceviord.Parameter) error {
 	var c *discordgo.ApplicationCommand
 	for _, rawC := range s.cmds {
 		if rawC.Name == changeCmdName {
@@ -58,109 +58,8 @@ func (s *SlashCmdGenerator) AddCastOpt(ps []ceviord.Parameter) error {
 	return nil
 }
 
-func (s *SlashCmdGenerator) Generate() []*discordgo.ApplicationCommand {
+func (s *Generator) Generate() []*discordgo.ApplicationCommand {
 	return s.cmds
-}
-
-var cmds = []*discordgo.ApplicationCommand{
-	{
-		Name:        joinCmdName,
-		Description: "join voice actor",
-	},
-	{
-		Name:        byeCmdName,
-		Description: "voice actor disconnect",
-	},
-	{
-		Name:        helpCmdName,
-		Description: "get command reference",
-	},
-	{
-		Name:        pingCmdName,
-		Description: "check connection status",
-	},
-	{
-		Name:        dictCmdName,
-		Description: "manage dict records",
-		Options: []*discordgo.ApplicationCommandOption{
-			{
-				Name:        "add",
-				Description: "add record",
-				Type:        discordgo.ApplicationCommandOptionSubCommand,
-				Options: []*discordgo.ApplicationCommandOption{
-					{
-						Name:        "word",
-						Description: "word",
-						Type:        discordgo.ApplicationCommandOptionString,
-						Required:    true,
-					},
-					{
-						Name:        "yomi",
-						Description: "how to read that word",
-						Type:        discordgo.ApplicationCommandOptionString,
-						Required:    true,
-					},
-				},
-			},
-			{
-				Name:        "del",
-				Description: "delete record",
-				Type:        discordgo.ApplicationCommandOptionSubCommand,
-				Options: []*discordgo.ApplicationCommandOption{
-					{
-						Name:        "id",
-						Description: "dictionary record id",
-						Type:        discordgo.ApplicationCommandOptionInteger,
-						Required:    true,
-					},
-				},
-			},
-			{
-				Name:        "show",
-				Description: "show records",
-				Type:        discordgo.ApplicationCommandOptionSubCommand,
-				Options: []*discordgo.ApplicationCommandOption{
-					{
-						Name:        "length",
-						Description: "specify number of records",
-						Type:        discordgo.ApplicationCommandOptionInteger,
-						Required:    false,
-					},
-				},
-			},
-			{
-				Name:        "dump",
-				Description: "dump all records",
-				Type:        discordgo.ApplicationCommandOptionSubCommand,
-			},
-			/* {
-				Name:        "search",
-				Description: "search record with effect",
-				Type:        discordgo.ApplicationCommandOptionSubCommand,
-				Options: []*discordgo.ApplicationCommandOption{
-					{
-						Name:        "search string",
-						Description: "search",
-						Type:        discordgo.ApplicationCommandOptionString,
-						Required:    true,
-					},
-				},
-			}, */
-		},
-	},
-	{
-		Name:        changeCmdName,
-		Description: "change voice actor",
-		Options: []*discordgo.ApplicationCommandOption{
-			{
-				Name:        "cast",
-				Description: "cast name",
-				Type:        discordgo.ApplicationCommandOptionString,
-				Choices:     []*discordgo.ApplicationCommandOptionChoice{},
-				Required:    true,
-			},
-		},
-	},
 }
 
 func InteractionHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
