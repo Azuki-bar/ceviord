9572036514b0652c31644585bf2cb86f89ea3673
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2023-01-09 16:09:07 +0900

    fix envconfig tags

--- cmd/main.go
+++ cmd/main.go
@@ -36,7 +36,7 @@ type conf struct {
 }
 
 type logConf struct {
-	Level *zapcore.Level `envConfig:"default=''"`
+	Level *zapcore.Level `envConfig:"default=,optional"`
 }
 
 func getConf() (*conf, error) {
